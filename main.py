91ca4aac8167e38b63969dbdd76a5c9a8157cb2c
Bill <417060833@qq.com>
2024-03-05 19:04:23 +0800

    upgrade version v2

--- main.py
+++ main.py
@@ -14,6 +14,37 @@ ARGOCD_PASSWORD=os.getenv("ARGOCD_PASSWORD")
 ARGOCD_APPNAME=os.getenv("ARGOCD_APPNAME")
 
 
+def extract_values_from_string(input_string):
+    """
+    Extracts the 'repo', 'path', and 'target' values from a given string formatted in a specific way.
+
+    Args:
+    input_string (str): A string containing various fields separated by spaces.
+
+    Returns:
+    tuple: A tuple containing the 'repo', 'path', and 'target' values.
+    """
+    # Split the string by spaces
+    parts = input_string.split()
+    if len(parts) < 4:
+        return "", "", "", ""
+    logging.debug(f"extract_values_from_string: {parts}")
+
+    # The expected positions of the 'repo', 'path', and 'target' in the parts list
+    # Assuming the format is consistent and these fields are always in the same position
+    repo_index = -3
+    path_index = -2
+    target_index = -1
+    name_index = 0
+
+    repo = parts[repo_index]
+    path = parts[path_index]
+    target = parts[target_index]
+    name = parts[name_index]
+
+    return name, repo, path, target
+
+
 def extract_path_from_url(url):
     # 分割 URL
     parts = url.split('/')
@@ -86,8 +117,57 @@ def get_webhook():
     return jsonify("success.")
 
 
+@app.route('/v2/api/webhook', methods=['POST'])
+def get_webhook_v2():
+    logging.debug(f"request data: {request.data}")
+    if not request.data:  # 检测是否有数据
+        return jsonify('request webhook request failed. data is empty'), 500
+
+    data = request.data.decode('utf-8')
+    # 获取到POST过来的数据
+    data_json = json.loads(data)
+    # get branch
+    ref = data_json["ref"]
+    branch = ref.split('/')[-1]
+    logging.debug(f"get branch is {branch}")
+    # get repo
+    repo_uri = extract_path_from_url(data_json["repository"]["url"])
+    repo_full_url = data_json["repository"]["url"]
+    # get directory list
+    commits = data_json["commits"]
+    dirname_list = set()
+    for commit in commits:
+        modifieds = commit["modified"]
+        for modified in modifieds:
+            dirname_list.add(modified.split('/')[0])
+    # 先登录
+    cmd(f"{ARGOCD_BIN_PATH}", "login", f"{ARGOCD_SERVER_HOST}", "--username", f"{ARGOCD_USERNAME}", "--password",
+        f"{ARGOCD_PASSWORD}", "--insecure")
+    # 项目路径
+    logging.debug(f"get branch={branch}, dirname_list={','.join(dirname_list)}")
+    argocd_app_list = cmd(f"{ARGOCD_BIN_PATH}", "app", "list")
+    logging.debug(f"get app list is: {argocd_app_list[:1000]}")
+    sync_app_list = []
+    for app_line in argocd_app_list.split('\n'):
+        logging.debug(f"get app_line is {app_line}")
+        app_name,app_repo,app_dirname,app_repo_branch = extract_values_from_string(app_line)
+        if app_repo == repo_uri and app_dirname in dirname_list and app_repo_branch == branch:
+            logging.info(f"prepare sync app: {app_name}, dirname: {app_dirname}, branch: {branch}, repo: {repo_full_url}")
+            sync_app_list.append(app_name)
+    if len(sync_app_list) > 0:
+        logging.info(f"argocd sync app = {sync_app_list}.")
+        cmd(f"{ARGOCD_BIN_PATH}", "app", "sync", *sync_app_list)
+    else:
+        logging.info(f"argocd sync app = {sync_app_list}. nothing to sync")
+    return jsonify("success.")
+
+
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
+    log_level = os.getenv("log_level", "info")
+    if log_level == "debug":
+        logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
+    else:
+        logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
     app.run(debug=True, port="8080", host="0.0.0.0")
